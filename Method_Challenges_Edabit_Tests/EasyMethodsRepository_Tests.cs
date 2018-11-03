603167e023c15945275e9ed59afcef01d7411ea9
Lawrence Wagner <lawrencewagner77@gmail.con>
2018-11-03 13:53:40 -0400

    ReturnTrue first Challenge complete with Unit Test

---
+++ Method_Challenges_Edabit_Tests/EasyMethodsRepository_Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Method_Challenges_EdaBit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Method_Challenges_Edabit_Tests
+{
+    [TestClass]
+    public class EasyMethodsRepository_Tests
+    {
+        EasyMethodsRepository _easyRepo = new EasyMethodsRepository();
+
+        [TestInitialize]
+        public void Arrange()
+        {
+
+        }
+
+        [TestMethod]
+        public void FirstChallenge_ReturnTrueBool_BoolShouldBeTrue()
+        {
+            //--Arrange
+            bool expected = true;
+            //--Act
+            bool actual = _easyRepo.ReturnTrue();
+            //--Assert
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
--- Method_Challenges_Edabit_Tests/MockConsole.cs
+++ Method_Challenges_Edabit_Tests/MockConsole.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Method_Challenges_EdaBit;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,49 @@ using System.Threading.Tasks;
 
 namespace Method_Challenges_Edabit_Tests
 {
-    class MockConsole
+    public class MockConsole : IConsole
     {
+        public Queue<string> UserInput;
+        public string Output;
+
+        public MockConsole() { }
+
+        public MockConsole(IEnumerable<string> input)
+        {
+            UserInput = new Queue<string>(input);
+            Output = string.Empty;
+        }
+
+        public void Clear()
+        {
+            Output += "Called Clear Method";
+        }
+
+        public ConsoleKeyInfo ReadKey()
+        {
+            return new ConsoleKeyInfo();
+        }
+
+        public string ReadLine()
+        {
+            if (UserInput.Count == 0)
+                return "";
+            return UserInput.Dequeue();
+        }
+
+        public void Write(string s)
+        {
+            Output += s;
+        }
+
+        public void WriteLine(string s)
+        {
+            Output += s + "\n";
+        }
+
+        public void WriteLine(object o)
+        {
+            Output += o.ToString() + "\n";
+        }
     }
 }
